2093ec6cc02db82a8160f9287c5ce7986a6caa9f
voronovski <avoronovski@gmail.com>
2023-03-28 21:04:25 -0700

    Initial Commit

---
+++ ColorSlidersSwiftUI/ColorSlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersSwiftUIApp.swift
+//  ColorSlidersSwiftUI
+//
+//  Created by Aleksei Voronovskii on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
